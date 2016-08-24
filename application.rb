17017f213a91647ea43fec42b74c3287d6c1b123
Alex Berry <berry.alexanderlee@gmail.com>
2016-08-24 17:21:45 +0100

    second test pass

--- application.rb
+++ application.rb
@@ -1,12 +1,27 @@
 class Person
 require 'date'
 	attr_accessor :first_name, :surname, :dob
-	attr_reader :fullname
+	attr_reader :fullname,:emails,:phone_numbers
+
 	def initialize(name,sname,dob)
 		@first_name = name.capitalize
 		@surname = sname.capitalize
 		@dob = Date.parse(dob)
 		@fullname = @first_name + " " + @surname
+		@emails = []
+		@phone_numbers = []
+	end
+
+	def add_emails(input_email)
+		@emails.push(input_email)
+	end
+
+	def add_phones(input_phone_number)
+		@phone_numbers.push(input_phone_number)
+	end
+
+	def remove_emails(index)
+		@emails.delete_at(index)
 	end
 
 
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,14 +1,29 @@
 require 'spec_helper'
 
 describe "Person class - entry in phonebook" do
+
+	before(:each) do 
+		@person = Person.new("joe", "bloggs", "1 Jan 1996")
+	end
   it "should store and return personal information" do
-    person = Person.new("joe", "bloggs", "1 Jan 1996")
-
+    
     # # Expected Outcomes
-    expect(person.dob.to_s).to eq "1996-01-01"
-
-    expect(person.first_name).to eq "Joe"
-    expect(person.surname).to eq "Bloggs"
-    expect(person.fullname).to eq "Joe Bloggs"
+    expect(@person.dob.to_s).to eq "1996-01-01"
+    expect(@person.first_name).to eq "Joe"
+    expect(@person.surname).to eq "Bloggs"
+    expect(@person.fullname).to eq "Joe Bloggs"
   end
+  it "should store mutiple emails and phone numbers" do 
+  	@person.add_emails ("joe@foo.com")
+	@person.add_emails ("joe.bloggs@work.com")
+	@person.add_phones ("07712345678")
+	@person.add_phones ("02012345678")
+
+ 	expect(@person.emails).to eq ["joe@foo.com", "joe.bloggs@work.com"]
+  	expect(@person.phone_numbers).to eq ["07712345678", "02012345678"]
+
+  	@person.remove_emails(0)
+	expect(@person.emails).to eq ["joe.bloggs@work.com"]
+	end
 end
+
