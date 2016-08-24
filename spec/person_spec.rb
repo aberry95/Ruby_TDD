e74803877f7101bd3168f6087ca1783fde3bafe0
Alex Berry <berry.alexanderlee@gmail.com>
2016-08-24 17:24:38 +0100

    second test pass

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
 describe "Person class - entry in phonebook" do
-
 	before(:each) do 
 		@person = Person.new("joe", "bloggs", "1 Jan 1996")
 	end
   it "should store and return personal information" do
-    
-    # # Expected Outcomes
     expect(@person.dob.to_s).to eq "1996-01-01"
     expect(@person.first_name).to eq "Joe"
     expect(@person.surname).to eq "Bloggs"
@@ -18,10 +15,8 @@ describe "Person class - entry in phonebook" do
 	@person.add_emails ("joe.bloggs@work.com")
 	@person.add_phones ("07712345678")
 	@person.add_phones ("02012345678")
-
  	expect(@person.emails).to eq ["joe@foo.com", "joe.bloggs@work.com"]
   	expect(@person.phone_numbers).to eq ["07712345678", "02012345678"]
-
   	@person.remove_emails(0)
 	expect(@person.emails).to eq ["joe.bloggs@work.com"]
 	end
