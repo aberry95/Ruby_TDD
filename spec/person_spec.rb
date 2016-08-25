1a17fd3176ca3cd2ccef4f20be4f5860ebc88b58
Alex Berry <berry.alexanderlee@gmail.com>
2016-08-25 10:28:50 +0100

    Cleaned up code

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,14 +4,13 @@ describe "Person class - entry in phonebook" do
 	before(:each) do 
 		@person = Person.new("joe", "bloggs", "1 Jan 1996")
 	end
-	it "should store and return personal information" do
+	it "Should store and return personal information" do
 		expect(@person.dob.to_s).to eq "1996-01-01"
 		expect(@person.first_name).to eq "Joe"
 		expect(@person.surname).to eq "Bloggs"
 		expect(@person.fullname).to eq "Joe Bloggs"
 	end
-	it "should store mutiple emails and phone numbers" do 
-
+	it "Should store mutiple emails and phone numbers" do 
 		@person.add_emails ("joe@foo.com")
 		@person.add_emails ("joe.bloggs@work.com")
 		@person.add_phones ("07712345678")
@@ -24,7 +23,7 @@ describe "Person class - entry in phonebook" do
 end
 
 describe "FamilyMember class - entry in phonebook" do
-	it "should store and return personal information of relative" do
+	it "Should store and return personal information of relative" do
 		family = FamilyMember.new("bo","derek","6 Jun 1995", "Uncle")
 		expect(family.dob.to_s).to eq "1995-06-06"
 		expect(family.first_name).to eq "Bo"
@@ -35,7 +34,7 @@ describe "FamilyMember class - entry in phonebook" do
 end
 
 describe "PhoneBook class -  adding contents in phonebook" do
-	it "should store and return personal information of relative" do
+	it "Should be able to add contents to  the phonebook" do
 		book = PhoneBook.new
 		person1 = Person.new("joe", "bloggs", "1 Jan 1990")
 		person2 = Person.new("andy", "nother", "2 Jan 1995")
